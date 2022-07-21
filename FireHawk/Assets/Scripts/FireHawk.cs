1faa84b9efbd917b4fc0267e78a6cf8587bc420c
Luis Guijarro Pérez <luisgp82@gmail.com>
2022-07-21 02:00:43 +0200

    Define FireHawk horizontal move.

--- FireHawk/Assets/Scripts/FireHawk.cs
+++ FireHawk/Assets/Scripts/FireHawk.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class FireHawk : MonoBehaviour
 {
+    [SerializeField] private float angleRotZ;
+    [SerializeField] private readonly float rotSpeed = 10f;
+    [SerializeField] private readonly float maxAngle = 45;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,48 @@ public class FireHawk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //float horMov = Input.
+        float horMov = Input.GetAxis("Horizontal");
+
+        // Translate
+        this.transform.Translate(Vector3.right * horMov, Space.World);
+        
+        if (this.transform.position.x > 44)
+        {
+            this.transform.position = new Vector3(44, this.transform.position.y, this.transform.position.z);
+        }
+        else if (this.transform.position.x < -44)
+        {
+            this.transform.position = new Vector3(-44, this.transform.position.y, this.transform.position.z);
+        }
+
+        // Rotate Efect...
+
+        this.angleRotZ += horMov * (this.rotSpeed / 30f);
+
+        if (angleRotZ > 1)
+        {
+            angleRotZ -= (rotSpeed ) * Time.deltaTime;
+        }
+        else if (angleRotZ < -1)
+        {
+            angleRotZ += (rotSpeed ) * Time.deltaTime;
+        }
+        else
+        {
+            angleRotZ = 0;
+        }
+
+
+        if (angleRotZ > maxAngle)
+        {
+            angleRotZ = maxAngle;
+        }
+
+        if (angleRotZ < -maxAngle)
+        {
+            angleRotZ = -maxAngle;
+        }
+
+        this.transform.rotation = Quaternion.AngleAxis(((int)-angleRotZ), Vector3.forward);
     }
 }

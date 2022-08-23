ed17ebb1f69d0327db4407f451c74e362ceb3068
Luis Guijarro Pérez <luisgp82@gmail.com>
2022-08-23 02:02:59 +0200

    Added logical functionality for the initial enemy type.

---
+++ FireHawk/Assets/Scripts/navX01.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+public class navX01 : SpaceShip
+{
+    protected override void OnStart()
+    {
+        base.OnStart();
+        // Shoot();
+    }
+
+}

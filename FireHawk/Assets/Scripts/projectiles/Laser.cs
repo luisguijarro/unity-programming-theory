3674033273c6904751b502e92101f6618be22573
Luis Guijarro Pérez <luisgp82@gmail.com>
2022-08-23 02:41:48 +0200

    Base Script for Laser Projectile. May will be innecesary.

---
+++ FireHawk/Assets/Scripts/projectiles/Laser.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : projectile
+{
+    protected override void OnStart()
+    {
+        base.OnStart();
+        //this.damage = 1;
+    }
+}
